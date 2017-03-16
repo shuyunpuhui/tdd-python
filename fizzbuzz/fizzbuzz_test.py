3017576caf24a1402d04ba9690dd50d545c4f369
yunbo <wangyunbo@digcredit.com>
2017-03-16 17:58:56 +0800

    fizzbuzz 第3步: 写一个测试

--- fizzbuzz/fizzbuzz_test.py
+++ fizzbuzz/fizzbuzz_test.py
@@ -6,3 +6,5 @@ import fizzbuzz
 class FizzBuzzTest(unittest.TestCase):
     def test_should_return_1_if_input_is_1(self):
         self.assertEqual("1", fizzbuzz.calculate(1))
+
+    def test_should_return_the_number_if_number_is_normal(self):
