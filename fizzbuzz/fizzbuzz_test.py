5ad027b5450100e6f91cf3d3e74281b66ea1d1b2
yunbo <wangyunbo@digcredit.com>
2017-03-16 18:24:10 +0800

    fizzbuzz 第3步: 实现第4个功能

--- fizzbuzz/fizzbuzz_test.py
+++ fizzbuzz/fizzbuzz_test.py
@@ -9,3 +9,17 @@ class FizzBuzzTest(unittest.TestCase):
 
     def test_should_return_the_number_if_number_is_normal(self):
         self.assertEqual("11", fizzbuzz.calculate(11))
+
+    def test_should_return_fizz_if_input_is_3(self):
+        self.assertEqual("fizz", fizzbuzz.calculate(3))
+
+    def test_should_return_fizz_if_input_is_dividable_by_3(self):
+        self.assertEqual("fizz", fizzbuzz.calculate(9))
+
+    def test_should_return_buzz_if_input_is_dividable_by_5(self):
+        self.assertEqual("buzz", fizzbuzz.calculate(10))
+
+    def test_should_return_fizzbuzz_if_dividable_by_3_and_5(self):
+        self.assertEqual("fizzbuzz", fizzbuzz.calculate(15))
+
+
