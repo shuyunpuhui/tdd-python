b821077ff660a3604e9bc6c83ae0b8b91868e52a
guoyafei@digcredit.com <guoyafei@digcredit.com>
2017-03-21 17:56:17 +0800

    ddd

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,2 +1,7 @@
 def calculate(input_number):
-    return '1'
+    if input_number % 3 == 0:
+        return "fizz"
+    if input_number % 5 == 0:
+        return 'buzz'
+    else:
+        return str(input_number)
--- fizzbuzz/fizzbuzz_test.py
+++ fizzbuzz/fizzbuzz_test.py
@@ -8,3 +8,8 @@ class FizzBuzzTest(unittest.TestCase):
         self.assertEqual("1", fizzbuzz.calculate(1))
 
     def test_should_return_the_number_if_number_is_normal(self):
+        self.assertEqual('2', fizzbuzz.calculate(2))
+
+    def test_should_return_fizz_if_number_is_3(self):
+        self.assertEqual('fizz', fizzbuzz.calculate(3))
+        self.assertEqual('buzz', fizzbuzz.calculate(5))
