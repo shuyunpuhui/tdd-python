ad8f4eba94e1716811de76f6b6e295f8b848ffd1
guoyafei@digcredit.com <guoyafei@digcredit.com>
2017-03-23 20:09:53 +0800

    ff

--- string_calculator/string_calculator_test.py
+++ string_calculator/string_calculator_test.py
@@ -18,4 +18,19 @@ class StingCalculatorTest(unittest.TestCase):
     def test_should_return_sum_if_input_is_two_string_number(self):
         self.assertEqual(5, string_calulator.calculate('2,3'))
 
+    def test_should_return_sum_if_input_is_more_string_number(self):
+        self.assertEqual(14, string_calulator.calculate('2,3,4,5'))
+
+    def test_should_return_sum_if_input_is_more_string_number_with_delimiter_n(self):
+        self.assertEqual(6, string_calulator.calculate('1\n2,3'))
+
+    def test_should_return_sum_if_input_is_more_string_number_with_self_define_delimiter(self):
+        self.assertEqual(3, string_calulator.calculate('//;\n1;2'))
+
+    def test_should_show_the_number_when_if_negative(self):
+        with self.assertRaises(Exception) as context:
+            string_calulator.calculate('//;\n-1;2;-9')
+        self.assertIn('-1', context.exception.message)
+        self.assertIn('-9', context.exception.message)
+        self.assertIn('-91', context.exception.message)
 
--- string_calculator/string_calulator.py
+++ string_calculator/string_calulator.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
-
+import re
 def calculate(input_number):
-    if input_number == '':
+
+    if not input_number:
         return 0
-    return sum(int(i) for i in input_number.split(','))
+    number_list = []
+    negatives_list = []
+    if input_number.startswith('//') and '\n' in input_number:
+        input_number = input_number.split('\n')
+        delimiter = input_number[0][-1]
+        number_list = input_number[1].split(delimiter)
+    elif '\n' in input_number:
+        input_number = input_number.replace('\n', ',')
+        number_list = input_number.split(',')
+    if not number_list:
+        number_list = input_number.split(',')
+    for i in number_list:
+        if int(i) < 0:
+            negatives_list.append(i)
+    if negatives_list:
+        raise Exception('negatives not allowed:%s' % ','.join(negatives_list))
+    number_list = map(lambda x: int(x), number_list)
+    return sum(number_list)
