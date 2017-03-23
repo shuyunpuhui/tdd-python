ef21261930e30b7fa93ae7374c9bc59103ddf974
sunyoubo <sunyoubo@digcredit.com>
2017-03-23 15:30:33 +0800

    1.添加tdd练习

--- string_calculator/string_calculator.py
+++ string_calculator/string_calculator.py
@@ -2,7 +2,7 @@
 def add(input_string):
     cnt = 0
     if input_string:
-        input_list = input_string.split(",")
+        input_list = input_string.replace("\n", ',').strip().split(",")
         if len(input_list) == 1:
             cnt = int(input_list[0])
         else:
--- string_calculator/test.py
+++ string_calculator/test.py
@@ -15,3 +15,9 @@ class StringCalculatorTest(unittest.TestCase):
 
     def test_should_return_sum_if_input_two(self):
         self.assertEqual(3, string_calculator.add("1,2"))
+
+    def test_should_return_sum_if_input_more_then_tow(self):
+        self.assertEqual(10, string_calculator.add("1,2, 3, 4"))
+
+    def test_should_return_sum_if_input_has_space_inside_numbers(self):
+        self.assertEqual(6, string_calculator.add("1\n2,3"))
