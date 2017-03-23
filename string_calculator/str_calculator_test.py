bca894dea529a5e031eb892a425f0f523069b1ac
yunbo <wangyunbo@digcredit.com>
2017-03-23 18:59:20 +0800

    step 3

--- string_calculator/str_calculator_test.py
+++ string_calculator/str_calculator_test.py
@@ -9,3 +9,9 @@ class StrCalculatorTest(unittest.TestCase):
     def test_should_return_the_number_when_input_has_only_one_number(self):
         self.assertEqual(1, str_calculator.add("1"))
         self.assertEqual(50, str_calculator.add("50"))
+
+    def test_should_return_sum_when_input_has_2_numbers(self):
+        self.assertEqual(3, str_calculator.add("1,2"))
+
+    def test_should_return_sum_when_input_has_more_numbers(self):
+        self.assertEqual(15, str_calculator.add("1,2,3,4,5"))
